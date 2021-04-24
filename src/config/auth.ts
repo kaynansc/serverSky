0ca7981e315a5544ce7fc5351fe1679bb54954a9
kaynansc <kaynansc@gmail.com>
2021-04-24 01:22:32 -0300

    remove .env.test

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,9 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-});
-
 export default {
   jwt: {
     secret: process.env.APP_SECRET,
